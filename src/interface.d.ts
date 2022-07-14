21aae8775a3863b3172e00166fb20cc49cef8a17
pengjiahui.tomoya06 <pengjiahui.tomoya06@bytedance.com>
2022-07-14 21:17:23 +0800

    feat: 妈的 抄过来怎么完全变了样

---
+++ src/interface.d.ts
@@ -0,0 +1,7 @@
+export interface IVertices {
+  [index: string]: {
+    x: number;
+    y: number;
+    z: number;
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import makeCloud from "./assets/generate/cloud";
 
 /** 场景 & 相机 */
 const clock = new THREE.Clock();
@@ -16,7 +17,7 @@ camera.position.z = 10;
 camera.position.y = 6;
 camera.position.x = -10;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 /** 场景 & 相机 */
@@ -35,12 +36,24 @@ controls.object.position.set(
 );
 
 /** 灯光 */
-const light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
+// const light = new THREE.AmbientLight(0xffffff);
+// scene.add(light);
 
-const directionalLight = new THREE.DirectionalLight(0xff8c19);
-directionalLight.position.set(0, 0, 1);
-scene.add(directionalLight);
+// const directionalLight = new THREE.DirectionalLight(0xff8c19);
+// directionalLight.position.set(0, 0, 1);
+// scene.add(directionalLight);
+
+const light1 = new THREE.DirectionalLight(0xffffff, 0.7);
+light1.position.set(1, 1, 0).normalize();
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0xff5566, 0.7);
+light2.position.set(-3, -1, 0).normalize();
+scene.add(light2);
+scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+
+const fogColor = new THREE.Color(0xff5566);
+scene.fog = new THREE.Fog(fogColor, 1, 200);
 /** 灯光 */
 
 /* 加载模型 */
@@ -64,38 +77,37 @@ function animatePlane() {
 }
 
 // 云
-const NumsOfCloudModels = 1;
-const cloudModelUrls = Array(NumsOfCloudModels)
-  .fill(0)
-  .map((_, idx) => `src/assets/models/polyclouds/AnyConv.com__Cloud_${idx+1}.gltf`);
+// const NumsOfCloudModels = 1;
+// const cloudModelUrls = Array(NumsOfCloudModels)
+//   .fill(0)
+//   .map((_, idx) => `src/assets/models/polyclouds/AnyConv.com__Cloud_${idx+1}.gltf`);
 
-// const cloudMetrial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-const cloudModels = await Promise.all(
-  cloudModelUrls.map(async (url) => {
-    const model = await new GLTFLoader().loadAsync(url);
-    return model;
-  })
-);
+// // const cloudMetrial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+// const cloudModels = await Promise.all(
+//   cloudModelUrls.map(async (url) => {
+//     const model = await new GLTFLoader().loadAsync(url);
+//     return model;
+//   })
+// );
 
-const cloudInstances: THREE.Group[] = [];
+const cloudInstances: THREE.Mesh[] = [];
 const numsOfClouds = 100;
 for (let i = 0; i < numsOfClouds; i++) {
   createNewCloud();
 }
 
 function createNewCloud() {
-  const pickedIdx = Math.floor(Math.random() * NumsOfCloudModels);
-  const pickedModel = cloudModels[pickedIdx];
-  const newCloud = pickedModel.scene.clone();
-  newCloud.scale.set(1, 1, 1);
-  newCloud.position.set(
-    Math.random() * 10 - 5,
-    1 + Math.random() * 0.2 - 0.1,
-    Math.random() * 100 - 10
-  );
+  const cloud = makeCloud();
 
-  scene.add(newCloud);
-  cloudInstances.push(newCloud);
+  const scale = Math.random() * 1.15 + 0.5;
+  cloud.position.x = Math.random() * 10 - 5;
+  cloud.position.z = Math.random() * 100 + 1;
+  cloud.position.y = Math.random() * 10 - 5;
+  cloud.rotation.y += Math.random() * 0.002 + 0.001;
+  cloud.scale.set(scale, scale, scale);
+
+  scene.add(cloud);
+  cloudInstances.push(cloud);
 }
 
 // 云层动画
