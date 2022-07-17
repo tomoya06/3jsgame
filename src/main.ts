1fc62229e37474785c9aed588dfca7f779cdaeec
tomoya06 <peng4766@gmail.com>
2022-07-17 23:02:03 +0800

    feat: 性能监控 微调

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import makeCloud0 from "./assets/generate/cloud0";
+import Stats from "stats.js";
 
 /** 场景 & 相机 */
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  60,
+  50,
   window.innerWidth / window.innerHeight,
   1,
   1000
 );
-camera.position.z = 10;
-camera.position.y = 6;
-camera.position.x = -10;
+camera.position.set(-20, 10, 0);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,17 +23,22 @@ document.body.appendChild(renderer.domElement);
 /** 场景 & 相机 */
 
 /** 辅助器 */
-const gridHelper = new THREE.GridHelper(10, 10);
+const gridHelper = new THREE.GridHelper(100, 10);
 scene.add(gridHelper);
 // 红色代表 X 轴. 绿色代表 Y 轴. 蓝色代表 Z 轴.
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.object.position.set(
-  camera.position.x,
-  camera.position.y,
-  camera.position.z
-);
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.object.position.set(
+//   camera.position.x,
+//   camera.position.y,
+//   camera.position.z
+// );
+
+// 性能监控
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
 
 /** 灯光 */
 const light1 = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -102,12 +107,16 @@ animate();
 function animate() {
   requestAnimationFrame(animate);
 
+  stats.begin();
+
   /** 动画帧更新 */
   animateCloud();
   animatePlane();
 
-  controls.update();
+  // controls.update();
   renderer.render(scene, camera);
+
+  stats.end();
 }
 
 function onWindowResize() {
