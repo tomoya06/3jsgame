a3141bb3af2afca869c15a1f8264c433ac645bfd
tomoya06 <peng4766@gmail.com>
2022-07-18 22:49:38 +0800

    feat: 多边形造云

--- src/assets/generate/cloud2.ts
+++ src/assets/generate/cloud2.ts
@@ -1,49 +1,30 @@
 import * as THREE from "three";
 
 export default function makeCloud2() {
-  const group = new THREE.Group();
+  const mesh = new THREE.Group();
 
-  const ballConfigs: {
-    radius: number;
-    detail?: number;
-    pos: [number, number];
-    rotate: [number, number];
-    scale?: [number, number, number];
-  }[] = [
-    {
-      radius: 1.0,
-      pos: [-0.4, -2],
-      rotate: [60, 45],
-    },
-    {
-      radius: 1.8,
-      pos: [0, 0],
-      rotate: [80, 60],
-    },
-    { radius: 1.1, pos: [-0.6, 2.0], rotate: [0, 0] },
-    { radius: 0.6, pos: [-0.8, 3.0], rotate: [40, 70], scale: [0.8, 0.8, 0.8] },
-  ];
-  const material = new THREE.MeshLambertMaterial({
+  const geom = new THREE.DodecahedronGeometry(1, 0);
+  const mat = new THREE.MeshPhongMaterial({
     color: 0xffffff,
   });
 
-  for (const config of ballConfigs) {
-    const cloudBall = new THREE.IcosahedronGeometry(
-      config.radius,
-      config.detail || 0
-    );
-    const ballMesh = new THREE.Mesh(cloudBall, material);
-    ballMesh.position.set(0, config.pos[0], config.pos[1]);
-    ballMesh.rotation.set(
-      0,
-      (config.rotate[0] / 180) * Math.PI,
-      (config.rotate[1] / 180) * Math.PI
-    );
-    if (config.scale) {
-      ballMesh.scale.set(config.scale[0], config.scale[1], config.scale[2]);
-    }
-    group.add(ballMesh);
+  const nBlocs = 3 + Math.floor(Math.random() * 3);
+
+  for (let i = 0; i < nBlocs; i++) {
+    //Clone mesh geometry
+    const m = new THREE.Mesh(geom, mat);
+    //Randomly position each cube
+    m.position.x = Math.random() * 0.4;
+    m.position.z = i;
+    m.position.y = Math.random() * 0.4;
+    m.rotation.z = Math.random() * Math.PI * 2;
+    m.rotation.y = Math.random() * Math.PI * 2;
+
+    //Randomly scale the cubes
+    const s = 0.1 + Math.random() * 0.9;
+    m.scale.set(s, s, s);
+    mesh.add(m);
   }
 
-  return group;
+  return mesh;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import makeCloud0 from "./assets/generate/cloud0";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import Stats from "stats.js";
+import makeCloud2 from "./assets/generate/cloud2";
 
 /** 场景 & 相机 */
 const clock = new THREE.Clock();
@@ -14,8 +15,9 @@ const camera = new THREE.PerspectiveCamera(
   1,
   1000
 );
-camera.position.set(-20, 10, 0);
+camera.position.set(-30, 20, 5);
 camera.lookAt(0, 0, 0);
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,12 +30,29 @@ scene.add(gridHelper);
 // 红色代表 X 轴. 绿色代表 Y 轴. 蓝色代表 Z 轴.
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.object.position.set(
-//   camera.position.x,
-//   camera.position.y,
-//   camera.position.z
-// );
+
+// 打坐标轴
+const fontMaterials = [
+  new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
+  new THREE.MeshPhongMaterial({ color: 0xffffff }), // side
+];
+const font = await new FontLoader().loadAsync(
+  "src/assets/fonts/helvetiker_regular.typeface.json"
+);
+[
+  [10, 0, 0],
+  [0, 10, 0],
+  [0, 0, 10],
+].map((ord) => {
+  const geo = new TextGeometry(`[${ord.join(",")}]`, {
+    font,
+  });
+  const mesh = new THREE.Mesh(geo, fontMaterials);
+  mesh.position.set(ord[0], ord[1], ord[2]);
+  mesh.scale.set(0.001, 0.001, 0.001);
+  mesh.rotateY((-90 / 180) * Math.PI);
+  scene.add(mesh);
+});
 
 // 性能监控
 const stats = new Stats();
@@ -42,12 +61,17 @@ document.body.appendChild(stats.dom);
 
 /** 灯光 */
 const light1 = new THREE.DirectionalLight(0xffffff, 1.0);
+const light1Helper = new THREE.DirectionalLightHelper(light1);
 light1.position.set(1, 1, 0).normalize();
 scene.add(light1);
+scene.add(light1Helper);
 
 const light2 = new THREE.DirectionalLight(0xff5566, 1.0);
 light2.position.set(-3, -1, 0).normalize();
+const light2Helper = new THREE.DirectionalLightHelper(light2);
 scene.add(light2);
+scene.add(light2Helper);
+
 scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 
 const fogColor = new THREE.Color(0xff5566);
@@ -60,7 +84,7 @@ const planeModel = await new GLTFLoader().loadAsync(
   "src/assets/models/cartoon_plane/scene.gltf"
 );
 
-const planeSize = 1;
+const planeSize = 2;
 planeModel.scene.scale.set(planeSize, planeSize, planeSize);
 scene.add(planeModel.scene);
 
@@ -76,21 +100,14 @@ function animatePlane() {
 
 // 云
 const cloudInstances: (THREE.Mesh | THREE.Group)[] = [];
-createNewCloud();
 
 async function createNewCloud() {
-  const newClouds = await makeCloud0(3);
-
-  for (const cloud of newClouds) {
-    const scale = Math.random() * 0.3 + 0.8;
-    cloud.scale.set(scale, scale, scale);
-    cloud.position.x = Math.random() * 10 - 5;
-    cloud.position.z = Math.random() * 10 - 5;
-    cloud.position.y = Math.random() * 4 - 2;
-
-    scene.add(cloud);
-    cloudInstances.push(cloud);
-  }
+  const newCloud = makeCloud2();
+  newCloud.position.z = 15;
+  newCloud.position.y = 2 - Math.random() * 4;
+  newCloud.position.x = (5 - Math.random() * 10) * 3;
+  cloudInstances.push(newCloud);
+  scene.add(newCloud);
 }
 
 // 云层动画
@@ -98,6 +115,16 @@ function animateCloud() {
   cloudInstances.forEach((cld) => {
     cld.position.z -= 0.01;
   });
+}
+
+let cloudDeltaCnt = 0;
+function generateClouds() {
+  if (Math.random() < 0.005) {
+    createNewCloud();
+    console.log("new cloud generated");
+  }
+
+  // TODO: 如果云层已经消失在左侧 则删掉
 }
 
 /* 加载模型 */
@@ -108,6 +135,9 @@ function animate() {
   requestAnimationFrame(animate);
 
   stats.begin();
+
+  /** 生成新元素 */
+  generateClouds();
 
   /** 动画帧更新 */
   animateCloud();
